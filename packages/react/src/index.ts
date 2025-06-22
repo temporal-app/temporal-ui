87f6ffa15f77f944f6d1a63ceac1292519adfc94
Andrey Patyuchenko <andrey.patyuchenko@gmail.com>
2025-06-22 12:45:16 +0200

    chore: export Card component from index

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,6 @@
 export * from './components/badge';
 export * from './components/button';
+export * from './components/card';
 export * from './components/loader';
 export * from './components/stack';
 export * from './components/text-input';

e5b920bc52e66bfa71783dac0cd3177db66394bf
Andrey Patyuchenko <andrey.patyuchenko@gmail.com>
2025-07-05 12:44:58 +0200

    chore: update tsup configuration to remove CommonJS format and adjust entry points

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -1,22 +1,18 @@
+// noinspection JSUnusedGlobalSymbols
+
 import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: [
 		"src/index.ts",
 		"src/styles.ts",
-		"src/components/avatar/index.ts",
-		"src/components/badge/index.ts",
-		"src/components/box/index.ts",
-		"src/components/button/index.ts",
-		"src/components/card/index.ts",
-		"src/components/field/index.ts",
-		"src/components/loader/index.ts",
-		"src/components/stack/index.ts",
-		"src/components/text-input/index.ts",
-		"src/components/textarea/index.ts",
+		"src/components/*/index.ts",
 	],
 	format: [
 		"esm"
+	],
+	noExternal: [
+		"@temporal-ui/core"
 	],
 	dts: true,
 	splitting: true,

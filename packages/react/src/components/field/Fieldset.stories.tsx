55a15e757fe42a52b1434d161dfe38314158517a
Andrey Patyuchenko <andrey.patyuchenko@gmail.com>
2025-10-11 00:32:52 +0200

    fix: update Storybook import for Fieldset stories to use react-vite

--- packages/react/src/components/field/Fieldset.stories.tsx
+++ packages/react/src/components/field/Fieldset.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Fieldset } from "./Fieldset";
 import { TextInput } from "../text-input";
 
